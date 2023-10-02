0958c0c27e162b906e50d8648f0a693de5c07897
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-10-02 19:56:40 -0400

    Added another field type organization

--- contacts/Contact.java
+++ contacts/Contact.java
@@ -5,28 +5,39 @@ import java.util.Set;
 import java.util.Iterator;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 class Contact {
-    private String firstName;
-    private String surName;
-    private String number;
+    protected String name;
+    protected String number;
+    protected LocalDateTime timeAdded;
+    protected LocalDateTime timeEdited;
+    protected static final LocalDate today;
 
     private static final Set<Contact> contacts = new LinkedHashSet<Contact>();
 
     private static final Pattern pattern1;
     private static final Pattern pattern2;
-    //private static Pattern pattern3;
+
 
     static {
         // First group is wrapped with parentheses
         pattern1 = Pattern.compile("\\+?\\([^\\W_]+\\)([- ][^\\W_]{2,})*");
         // Second group or no groups are wrapped with parentheses
         pattern2 = Pattern.compile("\\+?[^\\W_]+([- ]\\([^\\W_]{2,}\\))*([- ][^\\W_]{2,})*");
+        today = LocalDate.now();
     }
 
-    public Contact(String firstName, String surName) {
-        this.firstName = firstName;
-        this.surName = surName;
+    enum Type {
+        ORGANIZATION,
+        PERSON
+    }
+
+    Contact(String name) {
+        this.name = name;
+        this.timeAdded = LocalDateTime.now();
+        this.timeEdited = LocalDateTime.now();
         this.number = "";
 
         Contact.contacts.add(this);
@@ -34,12 +45,8 @@ class Contact {
 
     // Setters and getters methods
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setNumber(String number) {
@@ -50,12 +57,8 @@ class Contact {
         }
     }
 
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getSurName() {
-        return this.surName;
+    public String getName() {
+        return this.name;
     }
 
     public String getNumber() {
@@ -64,7 +67,7 @@ class Contact {
 
     // Phone number specific methods
 
-    private boolean hasNumber() {
+    protected boolean hasNumber() {
         return !this.number.isEmpty();
     }
 
@@ -81,6 +84,7 @@ class Contact {
         return Contact.contacts.size();
     }
 
+    /*
     static void edit(int record, String field, String valueOfField) {
         int recordIndexInContacts = record - 1;
 
@@ -90,22 +94,20 @@ class Contact {
         while (iter.hasNext()) {
             Contact contact = iter.next();
             if (i == recordIndexInContacts) {
-                switch (field) {
-                    case "name":
-                        contact.setFirstName(valueOfField);
-                        break;
-                    case "surname":
-                        contact.setSurName(valueOfField);
-                        break;
-                    case "number":
-                        contact.setNumber(valueOfField);
-                        break;
+                if (contact.getClass() == PersonContact.class) {
+                    PersonContact personContact = (PersonContact) contact;
+                    personContact.edit(field, valueOfField);
+                } else {
+                    OrganizationContact organizationContact = (OrganizationContact) contact;
+                    organizationContact.edit(field, valueOfField);
                 }
                 break;
             }
             i++;
         }
     }
+
+     */
 
     static void remove(int record) {
         int recordIndexInContacts = record - 1;
@@ -130,21 +132,243 @@ class Contact {
 
         while (iter.hasNext()) {
             Contact contact = iter.next();
-            System.out.println(i + ". " + contact.displayContact());
+            if (contact.getClass() == PersonContact.class) {
+                System.out.println(i + ". " + contact.getName() + " " + ((PersonContact) contact).getSurName());
+            } else {
+                System.out.println(i + ". " + contact.getName());
+            }
             i++;
         }
     }
 
-    private String displayContact() {
-        String output = this.firstName + " " + this.surName + ", ";
+    static Contact findContact(int record) {
+        int recordIndexInContacts = record - 1;
 
-        if (this.hasNumber()) {
-            output += this.number;
-        } else {
-            output += "[no number]";
+        int i = 0;
+        Iterator<Contact> iter = Contact.contacts.iterator();
+
+        while (iter.hasNext()) {
+            Contact contact = iter.next();
+            if (i == recordIndexInContacts) {
+                return contact;
+            }
+            i++;
         }
 
+        return null;
+    }
+
+
+}
+
+class PersonContact extends Contact {
+
+    private String surName;
+    private LocalDate birthDate;
+    private Gender gender;
+    private static final Pattern birthDatePattern;
+
+    static {
+        birthDatePattern = Pattern.compile("([1-9]\\d){4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])");
+    }
+
+    enum Gender {
+        M,
+        F
+    }
+
+    PersonContact(String name) {
+        super(name);
+    }
+
+    // Getters and setters
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getSurName() {
+        return this.surName;
+    }
+
+    public void setBirthDate(String birthDate) {
+        if (correctBirthDateFormat(birthDate) && today.isAfter(LocalDate.parse(birthDate))) {
+            this.birthDate = LocalDate.parse(birthDate);
+        } else {
+            System.out.println("Bad birth date!");
+            this.birthDate = null;
+        }
+    }
+
+    private boolean hasBirthDate() {
+        return this.birthDate != null;
+    }
+
+    private static boolean correctBirthDateFormat(String birthDate) {
+        Matcher matcher = PersonContact.birthDatePattern.matcher(birthDate);
+
+        return matcher.matches();
+    }
+
+    public LocalDate getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setGender(String gender) {
+        Gender value = findByName(gender);
+        if (value == null) System.out.println("Bad gender!");
+        this.gender = value;
+    }
+
+    private static Gender findByName(String name)  {
+        Gender output = null;
+        for (Gender gender: Gender.values()) {
+            if (gender.name().equalsIgnoreCase(name))
+                output = gender;
+            break;
+        }
         return output;
+    }
+
+    private boolean hasGender() {
+        return this.gender != null;
+    }
+
+    public Gender getGender() {
+        return this.gender;
+    }
+
+    protected void getInfo() {
+        String birthDate;
+        if (this.hasBirthDate()) {
+            birthDate = this.birthDate.toString();
+        } else {
+            birthDate = "[no data]";
+        }
+
+        String gender;
+        if (this.hasGender()) {
+            gender = this.gender.toString();
+        } else {
+            gender = "[no data]";
+        }
+
+        String number;
+        if (this.hasNumber()) {
+            number = this.number;
+        } else {
+            number = "[no number]";
+        }
+
+        System.out.println("Name: " + this.name + "\n" +
+                "Surname: " + this.surName + "\n" +
+                "Birth date: " + birthDate + "\n" +
+                "Gender: " + gender + "\n" +
+                "Number: " + number + "\n" +
+                "Time created: " + this.timeAdded + "\n" +
+                "Time last edit: " + this.timeEdited);
+    }
+
+    void edit(String field, String valueOfField) {
+        switch (field) {
+            case "name":
+                this.setName(valueOfField);
+                break;
+            case "surname":
+                this.setSurName(valueOfField);
+                break;
+            case "birth":
+                this.setBirthDate(valueOfField);
+                break;
+            case "gender":
+                this.setGender(valueOfField);
+                break;
+            case "number":
+                this.setNumber(valueOfField);
+                break;
+        }
+        this.timeEdited = LocalDateTime.now();
+    }
+
+}
+
+class OrganizationContact extends Contact {
+    private String address;
+
+    OrganizationContact(String name) {
+        super(name);
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    protected void getInfo() {
+        System.out.println("Organization name: " + this.name + "\n" +
+                "Address: " + this.address);
+
+        String number;
+        if (this.hasNumber()) {
+            number = this.number;
+        } else {
+            number = "[no number]";
+        }
+
+        System.out.println("Number: " + number + "\n" +
+                "Time created: " + this.timeAdded + "\n" +
+                "Time last edit: " + this.timeEdited);
+
+    }
+
+    void edit(String field, String valueOfField) {
+        switch (field) {
+            case "address":
+                this.setAddress(valueOfField);
+                break;
+            case "number":
+                this.setNumber(valueOfField);
+                break;
+        }
+        this.timeEdited = LocalDateTime.now();
+    }
+}
+
+class ContactStaticFactory {
+
+    public Contact newInstance(Contact.Type type, String name) {
+        switch (type) {
+            case PERSON -> {
+                return new PersonContact(name);
+            }
+            case ORGANIZATION -> {
+                return new OrganizationContact(name);
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+    public Contact setAttributes(Contact contact, String number, String surName, String birthDate, String gender) {
+        PersonContact personContact = (PersonContact) contact;
+        personContact.setSurName(surName);
+        personContact.setBirthDate(birthDate);
+        personContact.setGender(gender);
+        personContact.setNumber(number);
+
+        return personContact;
+    }
+
+    public Contact setAttributes(Contact contact, String number, String address) {
+        OrganizationContact organizationContact = (OrganizationContact) contact;
+        organizationContact.setAddress(address);
+        organizationContact.setNumber(number);
+
+        return organizationContact;
     }
 
 }
