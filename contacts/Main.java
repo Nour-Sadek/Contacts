d4fb4c35446f66620bc1df28859a7371f72cb5b0
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-10-09 22:53:28 -0400

    changed display of program

--- contacts/Main.java
+++ contacts/Main.java
@@ -1,8 +1,12 @@
 package contacts;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.*;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
@@ -11,6 +15,7 @@ public class Main {
     public static void main(String[] args) {
         String fileName = "phonebook.db";
 
+        // deserialize Contacts from phonebook.db
         try {
             Set<Contact> contacts = (Set<Contact>) SerializationUtils.deserialize(fileName);
             Contact.setContacts(contacts);
@@ -21,7 +26,7 @@ public class Main {
         }
 
         while (true) {
-            System.out.println("Enter action (add, remove, edit, count, info, exit):");
+            System.out.println("[menu] Enter action (add, list, search, count, exit):");
             String userInput = scanner.nextLine();
 
             if (userInput.equals("exit")) break;
@@ -30,16 +35,13 @@ public class Main {
                 case "add":
                     addRecord();
                     break;
-                case "remove":
-                    removeRecord();
-                    break;
-                case "edit":
-                    editRecord();
+                case "search":
+                    searchForRecords();
                     break;
                 case "count":
                     countRecords();
                     break;
-                case "info":
+                case "list":
                     infoOfRecords();
                     break;
             }
@@ -47,11 +49,94 @@ public class Main {
             System.out.println();
         }
 
+        // Serialize Contacts to phonebook.db
         try {
             SerializationUtils.serialize(Contact.getContacts(), fileName);
         } catch (IOException e) {
             String message = e.getMessage();
             System.out.println(message);
+        }
+    }
+
+    private static void searchForRecords() {
+        System.out.println("Enter search query:");
+        String query = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(query, Pattern.CASE_INSENSITIVE);
+        Matcher matcher;
+
+        Set<Contact> contacts = Contact.getContacts();
+        List<Contact> matchedContacts = new ArrayList<>();
+
+        for (Contact contact: contacts) {
+
+            matcher = pattern.matcher(contact.toString());
+
+            if (matcher.find()) {
+                matchedContacts.add(contact);
+            }
+
+        }
+
+        if (matchedContacts.isEmpty()) {
+            System.out.println("Found 0 results\n");
+        } else {
+            if (matchedContacts.size() == 1) System.out.println("Found 1 result:");
+            else System.out.println("Found " + matchedContacts.size() + " results:");
+
+            int i = 1;
+            for (Contact contact: matchedContacts) {
+                System.out.println(i + ". " + contact);
+                i++;
+            }
+            System.out.println();
+
+            searchMenu(matchedContacts);
+        }
+
+    }
+
+    private static void searchMenu(List<Contact> matchedContacts) {
+        System.out.println("[search] Enter action ([number], back, again):");
+        String userInput = scanner.nextLine();
+
+        if (userInput.equals("again")) searchForRecords();
+        else if (isInt(userInput)) {
+            int number = Integer.parseInt(userInput);
+            if (number > 0 && number <= matchedContacts.size()) {
+                Contact requiredContact = matchedContacts.get(number - 1);
+                requiredContact.getInfo();
+                recordMenu(requiredContact);
+            } else {
+                System.out.println("Incorrect record requested.");
+            }
+        }
+    }
+
+    private static void recordMenu(Contact requiredContact) {
+        System.out.println("[record] Enter action (edit, delete, menu):");
+        String userInput = scanner.nextLine();
+
+        switch (userInput) {
+            case "menu":
+                return;
+            case "edit":
+                requiredContact.editRecord();
+                recordMenu(requiredContact);
+                break;
+            case "delete":
+                Contact.remove(requiredContact);
+                break;
+        }
+
+    }
+
+    private static boolean isInt(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 
@@ -65,39 +150,35 @@ public class Main {
             System.out.println("Enter the name:");
             String name = scanner.nextLine();
             contact = contactStaticFactory.newInstance(type, name);
-            PersonContact personContact = (PersonContact) contact;
 
             System.out.println("Enter the surname:");
             String surName = scanner.nextLine();
-            personContact.setSurName(surName);
 
             System.out.println("Enter the birth date:");
             String birthDate = scanner.nextLine();
-            personContact.setBirthDate(birthDate);
 
-            System.out.println("Enter the gender:");
+            System.out.println("Enter the gender (M, F, ENBY):");
             String gender = scanner.nextLine();
-            personContact.setGender(gender);
 
             System.out.println("Enter the number:");
             String number = scanner.nextLine();
 
-            personContact.setNumber(number);
+            contactStaticFactory.setAttributes(contact, number, surName, birthDate, gender);
+
 
         } else {
             System.out.println("Enter the organization name:");
             String name = scanner.nextLine();
             contact = contactStaticFactory.newInstance(type, name);
-            OrganizationContact organizationContact = (OrganizationContact) contact;
 
             System.out.println("Enter the address:");
             String address = scanner.nextLine();
-            organizationContact.setAddress(address);
 
             System.out.println("Enter the number:");
             String number = scanner.nextLine();
 
-            organizationContact.setNumber(number);
+            contactStaticFactory.setAttributes(contact, number, address);
+
         }
 
         System.out.println("The recorded added.");
@@ -114,49 +195,6 @@ public class Main {
         return output;
     }
 
-    private static void removeRecord() {
-        if (Contact.count() == 0) {
-            System.out.println("No records to remove!");
-            return;
-        }
-
-        Contact.list();
-
-        System.out.println("Select a record:");
-        int record = Integer.parseInt(scanner.nextLine());
-        Contact.remove(record);
-
-        System.out.println("The record removed!");
-    }
-
-    private static void editRecord() {
-        if (Contact.count() == 0) {
-            System.out.println("No records to edit!");
-            return;
-        }
-
-        Contact.list();
-
-        System.out.println("Select a record:");
-        int record = Integer.parseInt(scanner.nextLine());
-        Contact contact = Contact.findContact(record);
-        if (contact.getClass() == PersonContact.class) {
-            System.out.println("Select a field (name, surname, birth, gender, number):");
-            String field = scanner.nextLine();
-            System.out.println("Enter " + field + ": ");
-            String valueOfField = scanner.nextLine();
-            ((PersonContact) contact).edit(field, valueOfField);
-        } else {
-            System.out.println("Select a field (address, number):");
-            String field = scanner.nextLine();
-            System.out.println("Enter " + field + ": ");
-            String valueOfField = scanner.nextLine();
-            ((OrganizationContact) contact).edit(field, valueOfField);
-        }
-
-        System.out.println("The record updated!");
-    }
-
     private static void countRecords() {
         System.out.println("The Phone Book has " + Contact.count() + " record(s).");
     }
@@ -169,14 +207,24 @@ public class Main {
 
         Contact.list();
 
-        System.out.println("Enter index to show info:");
-        int record = Integer.parseInt(scanner.nextLine());
-        Contact contact = Contact.findContact(record);
-        if (contact.getClass() == PersonContact.class) {
-            ((PersonContact) contact).getInfo();
-        } else {
-            ((OrganizationContact) contact).getInfo();
+        listMenu();
+
+    }
+
+    private static void listMenu() {
+        System.out.println("[list] Enter action ([number], back):");
+        String userInput = scanner.nextLine();
+
+
+        if (isInt(userInput)) {
+            int number = Integer.parseInt(userInput);
+            if (number > 0 && number <= Contact.getContacts().size()) {
+                Contact contact = Contact.findContact(number);
+                assert contact != null;
+                contact.getInfo();
+                recordMenu(contact);
+            }
         }
+
     }
 }
-
